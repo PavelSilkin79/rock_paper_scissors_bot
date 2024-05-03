ba3394db1bfdf26a248cdc7efe0c1f8be4a01ba8
pavel_silkin <pashasil-5@mail.ru>
2024-05-03 18:41:33 +0300

    five commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import asyncio
+import logging
+
+
+from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
+from rock_paper_scissors_bot.config_data.config import Config, load_config
+from handlers import other_handlers, user_handlers
+
+# Инициализируем логгер
+logger = logging.getLogger(__name__)
+
+
+# Создаем асинхронную функцию
+async def set_main_menu(bot: Bot):
+
+    # Создаем список с командами и их описанием для кнопки menu
+    main_menu_commands = [
+        BotCommand(command='/help',
+                   description='Справка по работе бота'),
+        BotCommand(command='/support',
+                   description='Поддержка'),
+        BotCommand(command='/contacts',
+                   description='Другие способы связи'),
+        BotCommand(command='/payments',
+                   description='Платежи')
+    ]
+
+    await bot.set_my_commands(main_menu_commands)
+
+
+
+
+# Функция конфигурирования и запуска бота
+async def main():
+    # Конфигурируем логирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
+
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting bot')
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    # Инициализируем бот и диспетчер
+    bot = Bot(token=config.tg_bot.token,
+              parse_mode='HTML')
+    dp = Dispatcher()
+
+    # Регистриуем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+    # Регистрируем асинхронную функцию в диспетчере,
+# которая будет выполняться на старте бота,
+    dp.startup.register(set_main_menu)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+asyncio.run(main())
