d0d27143261609abd7142aeafcc990dd7b2325c8
pavel_silkin <pashasil-5@mail.ru>
2024-05-03 20:04:22 +0300

    six commit

--- main.py
+++ main.py
@@ -3,31 +3,12 @@ import logging
 
 
 from aiogram import Bot, Dispatcher
-from aiogram.types import BotCommand
-from rock_paper_scissors_bot.config_data.config import Config, load_config
+from keyboards.set_menu import set_main_menu
+from config_data.config import Config, load_config
 from handlers import other_handlers, user_handlers
 
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
-
-
-# Создаем асинхронную функцию
-async def set_main_menu(bot: Bot):
-
-    # Создаем список с командами и их описанием для кнопки menu
-    main_menu_commands = [
-        BotCommand(command='/help',
-                   description='Справка по работе бота'),
-        BotCommand(command='/support',
-                   description='Поддержка'),
-        BotCommand(command='/contacts',
-                   description='Другие способы связи'),
-        BotCommand(command='/payments',
-                   description='Платежи')
-    ]
-
-    await bot.set_my_commands(main_menu_commands)
-
 
 
 
@@ -53,10 +34,9 @@ async def main():
     # Регистриуем роутеры в диспетчере
     dp.include_router(user_handlers.router)
     dp.include_router(other_handlers.router)
-    # Регистрируем асинхронную функцию в диспетчере,
-# которая будет выполняться на старте бота,
-    dp.startup.register(set_main_menu)
 
+        # Настраиваем кнопку Menu
+    await set_main_menu(bot)
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
